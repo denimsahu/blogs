1d5c8d1653b389cc4ab47d3d82b841b27f4a295c
Denim Sahu <denimsahu2003@gmail.com>
2023-08-15 15:03:40 +0530

    Commit Fourth - FixedSome Small  Bugs

--- Blogs/settings.py
+++ Blogs/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-i7=e)&yzezbu-3ee9$zzlqpb+xbwzs50tm=77aot_9^s7d-alk
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
